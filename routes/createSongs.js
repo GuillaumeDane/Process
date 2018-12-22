15d849c349f96d24223aa66637a3817c444c385b
Guillaume Dane <oxton@macbook-pro-de-guillaume.home>
2018-12-22 20:51:07 +0100

    +

--- routes/createSongs.js
+++ routes/createSongs.js
@@ -19,46 +19,46 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
   let music = new musicSchema();
 
-  music.name = req.body.name;
-  music.cover = req.body.cover;
-  music.producer = req.body.producer;
-  music.description = req.body.process;
-  music.bpm = req.body.bpm;
-  music.tonality = req.body.tonality;
+  music.name = req.body.name; // -> name
+  music.cover = req.body.cover; // -> cover
+  music.producer = req.body.producer; // -> producer
+  music.description = req.body.process; // -> description
+  music.bpm = req.body.bpm; // -> bpm
+  music.tonality = req.body.tonality; // -> tonality
 
 // ==== software post ==== //
 
-   music.software.name = req.body.software_name;
-   music.software.description = req.body.software_description;
-   music.software.img = req.body.software_img;
+   music.software.name = req.body.software_name; // -> software -> name
+   music.software.description = req.body.software_description; // -> software -> description
+   music.software.img = req.body.software_img; // -> software -> img
 
 // ==== vst post ==== //
 
-  music.vst_1.name = req.body.vst_name_1;
-  music.vst_1.img = req.body.vst_img_1;
+  music.vst_1.name = req.body.vst_name_1; // -> vst_1 -> name
+  music.vst_1.img = req.body.vst_img_1; // -> vst_1 -> img
 
-  music.vst_2.name = req.body.vst_name_2;
-  music.vst_2.img = req.body.vst_img_2;
+  music.vst_2.name = req.body.vst_name_2; // -> vst_2 -> name
+  music.vst_2.img = req.body.vst_img_2; // -> vst_2 -> img
 
-  music.vst_3.name = req.body.vst_name_3;
-  music.vst_3.img = req.body.vst_img_3;
+  music.vst_3.name = req.body.vst_name_3; // -> vst_3 -> name
+  music.vst_3.img = req.body.vst_img_3; // -> vst_3 -> img
 
-  music.vst_4.name = req.body.vst_name_4;
-  music.vst_4.img = req.body.vst_img_4;
+  music.vst_4.name = req.body.vst_name_4; // -> vst_4 -> name
+  music.vst_4.img = req.body.vst_img_4; // -> vst_4 -> img
 
 // ==== hardware post ==== //
 
-  music.hardware_1.name = req.body.hardware_name_1;
-  music.hardware_1.img = req.body.hardware_img_1;
+  music.hardware_1.name = req.body.hardware_name_1; // -> hardware_1 -> name
+  music.hardware_1.img = req.body.hardware_img_1; // -> hardware_1 -> img
 
-  music.hardware_2.name = req.body.hardware_name_2;
-  music.hardware_2.img = req.body.hardware_img_2;
+  music.hardware_2.name = req.body.hardware_name_2; // -> hardware_2 -> name
+  music.hardware_2.img = req.body.hardware_img_2; // -> hardware_2 -> img
 
-  music.hardware_3.name = req.body.hardware_name_3;
-  music.hardware_3.img = req.body.hardware_img_3;
+  music.hardware_3.name = req.body.hardware_name_3; // -> hardware_3 -> name
+  music.hardware_3.img = req.body.hardware_img_3; // -> hardware_3 -> img
 
-  music.hardware_4.name = req.body.hardware_name_4;
-  music.hardware_4.img = req.body.hardware_img_4;
+  music.hardware_4.name = req.body.hardware_name_4; // -> hardware_4 -> name
+  music.hardware_4.img = req.body.hardware_img_4; // -> hardware_4 -> img
 
   music.save(function(err) {
     if (err) {
